32007fb67b3ec687e39817414f3bea83eafe52d0
Brainfuck <brainfuck@protonmail.ch>
2016-05-24 10:25:06 +0200

    Version 2.1

--- kalieraser.sh
+++ kalieraser.sh
@@ -1,60 +1,64 @@
 #!/bin/bash
 
-##############################################################
-#							       							   						           	  
-#  Script: kalieraser.sh                                       
-#  							       							   						           	
-#  Version: 2.0 - 19/02/2016						           
-# 															   
-#  Dev: Brainfuck               		                   	           
-#  							       							   						           	
-#  Descr: Antiforensics script for security and privacy 
-#  
-#  Operative System: Kali Linux       
-#     							                               					  		   	
-#  Dependencies: Bleachbit > http://bleachbit.sourceforge.net/					  
-#                                                                  
-#  Secure RM > http://sourceforge.net/projects/srm/ 	       											          
-#                                                              
-##############################################################
+# Program: kalieraser.sh
+# Version: 2.1 - 23/05/2016
+# Dev: Brainfuck
+# Description: Antiforensics script for security and privacy
+# Operative System: Kali Linux 
+# Dependencies: Bleachbit > http://bleachbit.sourceforge.net
+# Secure RM > http://sourceforge.net/projects/srm/
+#
+# GNU GENERAL PUBLIC LICENSE
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-# Define Colors
-export GREEN='\033[1;92m'
-export RED='\033[1;91m'
-export WHITE='\033[1;97m'
-export CYAN='\033[1;96m'
+# define colors
+export green='\033[1;92m'
+export red='\033[1;91m'
+export white='\033[1;97m'
+export cyan='\033[1;96m'
 export RESETCOLOR='\033[1;00m'
-
 
 # banner
 function banner {
 cat << "EOF"
-   __ __     ___                         
-  / //_/__ _/ (_)__ _______ ____ ___ ____
- / ,< / _ `/ / / -_) __/ _ `(_-</ -_) __/
-/_/|_|\_,_/_/_/\__/_/  \_,_/___/\__/_/     	
-								
-Version: 2.0
-Dev: Brainfuck  
+ _____     _ _                         
+|  |  |___| |_|___ ___ ___ ___ ___ ___ 
+|    -| .'| | | -_|  _| .'|_ -| -_|  _|
+|__|__|__,|_|_|___|_| |__,|___|___|_|
+
+Version: 2.1
+Dev: Brainfuck
 EOF
 }
 
 
-# checkroot
-function checkroot {
+# check if root run the script 
+function check_root {
 	if [ "$(id -u)" -ne 0 ]; then
-		echo -e "\n$RED[!] Please run this script as a root!$RESETCOLOR\n" >&2
+		echo -e "\n$red[!] Please run this script as a root!$RESETCOLOR\n" >&2
 		exit 1
 	fi
 }
 
 
-# bleachbit command line interface 
-function runbleachbit {
-	echo -e "\n$CYAN[info] $GREEN Starting Bleachbit cleaner$RESETCOLOR\n"
+# run bleachbit command line interface
+function run_bleachbit {
+	echo -e "\n$cyan[info]$green Starting bleachbit cleaner$RESETCOLOR\n"
 	sleep 1
-	
+
 	bleachbit --overwrite --clean apt.autoremove apt.clean bash.history \ 
 	\ chromium.cache chromium.cookies chromium.dom chromium.form_history \
 	\ chromium.history chromium.passwords chromium.vacuum elinks.history \
@@ -76,143 +80,149 @@ function runbleachbit {
 	\ system.recent_documents system.rotated_logs system.tmp system.trash \
 	\ thumbnails.cache thunderbird.cache thunderbird.cookies thunderbird.passwords \
 	\ thunderbird.vacuum vim.history vlc.mru wine.tmp x11.debug_logs xchat.logs 
-	sleep 1	
-	clear
-	
-	echo -e ""
-	echo -e "$CYAN[+]$GREEN Temporary files are deleted$RESETCOLOR"
+	sleep 1
+	printf "\033c"
+
+	echo -e "\n$cyan[+]$green Temporary files are deleted$RESETCOLOR"
 }
 
 
 # delete logs with srm 
-function securerm {
-	# array for tools and logs folders 
-	declare -a logfolder=('/nmap_output/' '/.maltego/*BT/var/cache/*' 
-	'/.maltego/*BT/var/log/*' '/.maltego/*CaseFileCE/var/cache/*' 
-	'/.maltego/*CaseFileCE/var/cache/*' '*.mtgrx' '/.recon-ng/*' '*.sprt' 
-	'/*-tool-output/' '/.zenmap/*' '/.golismero/*' '/.ZAP/session/*' '/.ZAP/*.log/' 
-	'/paros/session/*' '*skipfish*' '/.sqlmap/output/*' '/.w3af/tmp/*' '*conversations*' 
-	'*fragments*' '*.properties' '*.data' '*.keystore' '*.pot' '.0trace-*' '/TLSSLed*/' 
-	'/.wapiti/*' 'fimap.log' 'hydra.restore' '/.creepy/*.log/' '.john/sessions/*.log'
-	'/.john/*' '/.set/*' '/.msf4/*' '/.wireshark/*' '/.faraday/logs/*' '/.armitage/*'
-	'/.weevely/*' '/var/log/dradis/*' '/var/log/openvas/*.log' '/var/log/openvas/*.gz' 
+function secure_rm {	
+	echo -e "\n$cyan[info]$green Starting secure file deletion with srm, this will take some time...$RESETCOLOR"
+	
+	# array for tools and logs folders
+	declare -a log_folder=(
+	# root and user Home directory
+	'/nmap_output/' '/.maltego/*BT/var/cache/*' '/.maltego/*BT/var/log/*' 
+	'/.maltego/*CaseFileCE/var/cache/*' '/.maltego/*CaseFileCE/var/cache/*'
+	'./chirp/*.log' '*.mtgrx' '/.recon-ng/*' '*.sprt' '/*-tool-output/' 
+	'/.zenmap/*' '/.golismero/*' '/.ZAP/session/*' '/.ZAP/*.log/' 
+	'/paros/session/*' '*skipfish*' '/.sqlmap/output/*' '/.w3af/tmp/*' 
+	'*conversations*' '*fragments*' '*.properties' '*.data' '*.keystore' 
+	'*.pot' '.0trace-*' '/TLSSLed*/' '/.wapiti/*' 'fimap.log' 'hydra.restore' 
+	'/.creepy/*.log/' '.john/sessions/*.log''/.john/*' '/.set/*' '/.msf4/*' 
+	'/.wireshark/*' '/.faraday/logs/*' '/.armitage/*' '/.weevely/*' 
+	# /var/log/
+	'/var/log/dradis/*' '/var/log/openvas/*.log' '/var/log/openvas/*.gz' 
 	'/var/log/openvas/*.dump' '/var/log/openvas/*.messages' '/var/log/*log' 
 	'/var/log/*.1' '/var/log/*.gz' '/var/log/*.old' '/var/log/*.err/' 
 	'/var/log/messages' 'var/log/mysql/*.log' '/var/log/mysql/*.gz' 
 	'/var/log/postgresql/*.log' '/var/log/wmtp' '/var/log/*.dat');
-	 
-	echo -e "\n$CYAN[info] $GREEN Starting secure file deletion with srm, this will take some time $RESETCOLOR\n"
-	sleep 2
-	
-	# root and user logs 
+
+	# root and user logs
 	IFS=$'\n'
-	for username in `echo -e "/root/\n$(ls /home/ | sed -e 's_^_/home/_' -e 's_$_/_')"`; do
-		echo -e "$CYAN[info] $GREEN Deleting logs of $WHITE$username $RESETCOLOR\n"
+	for username in $(echo -e "/root/\n$(ls /home/ | sed -e 's_^_/home/_' -e 's_$_/_')"); do
+		echo -e "\n$cyan[info]$green Deleting logs of $red$username$RESETCOLOR"
 		cd $username
 		sleep 2
-		
+
 		IFS=$','
-		for i in ${logfolder[@]}; do
-			if [ "$(ls -A "$i" 2>/dev/null | wc -l)" -gt 0 ]; then
-				# command srm -D --dod --> overwrite with 7 US DoD compliant passes
-				srm -D -R "$i" 2>/dev/null
-				echo -e "$CYAN[+] $GREEN deleted: $WHITE $i $RESETCOLOR"
+		for i in ${log_folder[@]}; do
+			if [ "$(ls -A "$i" 2> /dev/null | wc -l)" -gt 0 ]; then
+				# execute command "srm -i -D -R <file/folder>"
+				# -i, --interactive     prompt before any removal
+				# -D, --dod             overwrite with 7 US DoD compliant passes
+				# -r, -R, --recursive   remove the contents of directories
+				srm -i -D -R "$i" 2> /dev/null
+				echo -e "$cyan[+]$green deleted: $white $i $RESETCOLOR"
 			fi
 		done
 	done
+
+	# delete dmesg logs
+	echo -e "\n$green Delete kernel messages? [Y/n]:$RESETCOLOR"
+	read -e yno
+	case $yno in
+		[yY]|[y|Y] )
+			dmesg -C
+			sleep 3
+			echo -e "$cyan[+]$green dmesg deleted$RESETCOLOR\n"
+			;;
+		*)		
+			;;	
+	esac
 }
 
 
-# delete the last logs on system  
-function lastlogs {
-	# bleachbit don't remove root bash_history if you don't run command from sudo
-	# deleting for you with this function 
-	echo -e ""
-	echo -e "$CYAN[info] $GREEN Deleting dmesg and bash_history $RESETCOLOR\n"	
-	dmesg -C
-	history -c
-	
-	# drop data from RAM 
-	echo -e "$CYAN[info] $GREEN Drop data from RAM $RESETCOLOR"
-	echo 3 > /proc/sys/vm/drop_caches
-	sleep 3
-	clear
-	
-	echo -e "$CYAN[ ok ] $WHITE All the logs are secure deleted and System is clean$RESETCOLOR\n"			
+# emptying the buffers cache (free pagecache, dentries and inodes)
+function drop_cache {
+	echo -e "$cyan[info]$green Drop data from RAM$RESETCOLOR\n"
+	sh -c 'echo 3 >/proc/sys/vm/drop_caches'
+	echo -e "$cyan[+]$green RAM empty$RESETCOLOR"
+	sleep 5
+	printf "\033c"
+
+	echo -e "$cyan[ ok ]$white All logs and files are securely deleted and your System is clean$RESETCOLOR\n"
 }
 
 
-# print list of supported tools
-function list {
-	declare -a tools=('0trace' 'armitage' 'burpsuite' 'casefile' 'creepy' 'dradis'
-	'faraday' 'fimap' 'golismero''hashcat' 'hydra' 'hydra-gtk' 'lynis' 'maltego'
-	'metasploit' 'nmap' 'openvas' 'owasp-zap' 'p0f' 'paros''recon-ng' 'se-toolkit' 
-	'skipfish' 'sparta' 'sqlmap' 'ssldump' 'tlssled' 'unicornscan' 'uniscan' 
-	'uniscan-gui' 'w3af' 'wapiti' 'webscarab' 'weevely' 'wireshark' 'zenmap')
-	
-	banner
-	echo -e "\n$WHITE List of supported tools $RESETCOLOR\n"
-	printf '%s\n' "${tools[@]}" | more
-	exit 1 
-}
-
-
-# reboot system
-function systemreboot {
-	echo -e "\n$GREEN Stay safe and reboot system now? [Y/n]: $RESETCOLOR" 
-	read -e yno 
+# ask for reboot
+function system_reboot {
+	echo -e "\n$green It is recommended to reboot system, reboot now? [Y/n]:$RESETCOLOR"
+	read -e yno
 	case $yno in
 		[yY]|[y|Y] )
 			reboot
 			;;
 		*)
-		echo -e "\n$GREEN Exit ! $RESETCOLOR"
-			exit 1
-			;;				
+		echo -e "\n$green Exit!$RESETCOLOR"
+			exit 0
+			;;
 	esac
 }
 
 
-# start as main function
+# start program 
 function start {
-	banner 
-	checkroot
-	
-	# check if dependencies are installed
-	command -v bleachbit > /dev/null 2>&1 || 
-	{ echo -e "\n$RED [!] bleachbit isn't installed, exiting...$RESETCOLOR"; exit 0; }
-	command -v srm --help >/dev/null 2>&1 || 
-	{ echo -e "\n$RED [!] srm isn't installed, exiting...$RESETCOLOR"; exit 0; }
-	
-	runbleachbit
-	securerm 
-	lastlogs
-	systemreboot 
+	banner
+	check_root
+
+	# check if dependencies are installed 
+	command -v bleachbit -h > /dev/null 2>&1 ||
+	{ echo -e "\n$red[!] bleachbit isn't installed, exiting...$RESETCOLOR"; exit 0; }
+
+	command -v srm --help > /dev/null 2>&1 ||
+	{ echo -e "\n$red[!] srm isn't installed, exiting...$RESETCOLOR"; exit 0; }
+
+	run_bleachbit
+	secure_rm
+	drop_cache
+	system_reboot 
 }
 
 
-# simple start and list options 
+# print help menu'
+function help_menu {
+	banner 
+	echo -e "\n$white Usage:
+
+┌─╼ $red$USER$white ╺─╸ $red$(hostname)$white
+└───╼ $green""./kalieraser.sh $white[ $green""start$white | $green""help $white""]
+
+$white start$red -$green Start the program 
+
+$white help$red -$green Show this help message and exit 
+$RESETCOLOR" >&2
+	exit 1
+
+	echo -e $RESETCOLOR
+
+	exit 0
+}
+
+
+# menu options 
 case "$1" in
 	start)
 		start
-	;;
-	list)
-		list
-	;;
-   *)	
-
-# usage 
-banner 
-echo -e "\n$WHITE Usage:
-
-╭─[$RED$USER$WHITE]─[$RED`hostname`$WHITE]
-╰──> $GREEN""./kalieraser.sh $WHITE[$GREEN""start$WHITE | $GREEN""list$WHITE""]
-
-$WHITE start$GREEN --> Start the program 
-
-$WHITE list$GREEN  --> List the supported tools 
-
-$RESETCOLOR" >&2
-exit 1
+		;;
+	help)
+		help_menu
+		;;
+    *)
+help_menu
 ;;
 esac
+
+exit 0
