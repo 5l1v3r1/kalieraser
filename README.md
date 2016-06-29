15a911b3f762e49b6d3642323561234005b3dd18
BrainfuckSec <brainfuck@protonmail.ch>
2016-06-29 17:26:27 +0200

    version 2.2

--- README.md
+++ README.md
@@ -1,29 +1,27 @@
-### Kalieraser 
-
-##### Written by: Brainfuck
-##### Version: 2.1 - 23/05/2016
-##### Antiforensics script for security and privacy
-##### Operative System: Kali Linux 
-##### Description: This program erase all system's logs and data of kali tools, the files are wiped with Bleachbit (overwrite method) and Secure RM (7 US DoD compliant passes method).
+## Kalieraser v2.2
+ 
+### Antiforensics script for security and privacy
+### Operative System: Kali Linux 
+### Description: This program erase all system's logs and data of kali tools, the files are wiped with Bleachbit (overwrite method) and Secure RM (7 US DoD compliant passes method).
 
 
-#### Instructions
+### Instructions
 
 
-##### 1 - Install Bleachbit
-```
-apt-get install bleachbit 
+#### 1 - Install Bleachbit
+```bash
+sudo apt-get install bleachbit 
 ```
 
 
 
-##### 2 - Install srm 
+#### 2 - Install srm 
 
-##### Secure RM is already present in the program folder, but if you are paranoid you can install srm from here: http://sourceforge.net/projects/srm/files/. The version of srm in this repository is the latest and when the new version are released the git repository will be updated.
+##### Secure RM is already present in the program folder, this is the URL of srm project: http://sourceforge.net/projects/srm/files/. The version of srm in this repository is the latest and when the new version are released the git repository will be updated.
 
 
 ##### Extract and install: 
-```
+```bash
 tar -zxvf srm-1.2.15.tar.gz
 
 cd srm-1.2.15/
@@ -34,35 +32,32 @@ make
 
 sudo make install
 ```
+##### Now, the executable of srm is installed in /usr/local/bin directory. 
 
-##### 3 - Start the script from root  
+
+#### 3 - Start the script from root  
 
 ##### Chmod and start cleaner:
-```
+```bash
 chmod +x kalieraser.sh
 
 ./kalieraser.sh start 
 ```
 
 
-##### Print help menu 
-```
+#### Use help argument or run the program without arguments for help menu' 
+```bash
 ./kalieraser.sh help
 ```
 
 
-#### Note for the users:
+#### Note:
+[!] This is not a "magic security tool" and does not absolutely guarantee that data on the drive cannot be recovered, but it goes a long way in making it difficult.
 
-##### [!] This is not a "magic security tool" and does not absolutely guarantee that data on the drive cannot be recovered, but it goes a long way in making it difficult.
-
-#####  I could not insert some tools in the list because it was impossible for me to test all (i.e. the cisco tools and more).
+I could not insert some tools in the list because it was impossible for me to test all (i.e. the cisco tools and more).
 
 
-##### References:
-
-######  Bleachbit documentation: http://bleachbit.sourceforge.net/
-
-######  Secure RM documentation: http://srm.sourceforge.net/
-
-######  About secure wiping of media: http://www.destructdata.com/dod-standard/, https://en.wikipedia.org/wiki/Data_erasure
-
+#### References:
+Bleachbit documentation: http://bleachbit.sourceforge.net/
+Secure RM documentation: http://srm.sourceforge.net/
+About secure wiping of media: http://www.destructdata.com/dod-standard/, https://en.wikipedia.org/wiki/Data_erasure
--- kalieraser.sh
+++ kalieraser.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Program: kalieraser.sh
-#
-# Version: 2.1 - 23/05/2016
+# Version: 2.2 - 29/06/2016
 # Dev: Brainfuck
 # Description: Antiforensics script for security and privacy
-# Operative System: Kali Linux 
-# Dependencies: Bleachbit > http://bleachbit.sourceforge.net
-# Secure RM > http://sourceforge.net/projects/srm/
+# Operative System: Kali Linux
+# Dependencies Bleachbit, Secure RM
+# http://bleachbit.sourceforge.net
+# http://sourceforge.net/projects/srm/
 
 # GNU GENERAL PUBLIC LICENSE
 #
@@ -25,12 +25,17 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
+# program / version
+program="kalieraser"
+version="2.2"
+
 # define colors
-export green='\033[1;92m'
-export red='\033[1;91m'
-export white='\033[1;97m'
-export cyan='\033[1;96m'
-export RESETCOLOR='\033[1;00m'
+export green='\033[0;92m'
+export red='\033[0;91m'
+export white='\033[0;97m'
+export cyan='\033[0;96m'
+export endc='\e[0m'
+
 
 # banner
 function banner {
@@ -40,24 +45,99 @@ cat << "EOF"
 |    -| .'| | | -_|  _| .'|_ -| -_|  _|
 |__|__|__,|_|_|___|_| |__,|___|___|_|
 
-Version: 2.1
+Version: 2.2
 Dev: Brainfuck
 EOF
 }
 
 
-# check if root run the script 
+# check if the program run as a root
 function check_root {
 	if [ "$(id -u)" -ne 0 ]; then
-		echo -e "\n$red[!] Please run this script as a root!$RESETCOLOR\n" >&2
+		echo -e "$red[!] Please run this program as a root!$endc" >&2
 		exit 1
 	fi
 }
 
 
+# backup log files and folders 
+function backup_data {
+	banner
+	check_root
+	echo -e "\n$cyan[info]$green Backup log files and folders$endc"
+
+	local current_date=`date +%Y-%m-%d`
+
+	# list logs in root and user home directory  
+	declare -a log_folder=(
+	'/nmap_output/' '/.maltego/*BT/var/cache/*' '/.maltego/*BT/var/log/*' 
+	'/.maltego/*CaseFileCE/var/cache/*' '/.maltego/*CaseFileCE/var/cache/*'
+	'./chirp/*.log' '*.mtgrx' '/.recon-ng/*' '*.sprt' '/*-tool-output/' 
+	'/.zenmap/*' '/.golismero/*' '/.ZAP/session/*' '/.ZAP/*.log/' 
+	'/paros/session/*' '*skipfish*' '/.sqlmap/output/*' '/.w3af/tmp/*' 
+	'*conversations*' '*fragments*' '*.properties' '*.data' '*.keystore' 
+	'*.pot' '.0trace-*' '/TLSSLed*/' '/.wapiti/*' 'fimap.log' 'hydra.restore' 
+	'/.creepy/*.log/' '.john/sessions/*.log''/.john/*' '/.set/*' '/.msf4/*' 
+	'/.wireshark/*' '/.faraday/logs/*' '/.armitage/*' '/.weevely/*' 
+	# /var/log/
+	'/var/log/dradis/*' '/var/log/openvas/*.log' '/var/log/openvas/*.gz' 
+	'/var/log/openvas/*.dump' '/var/log/openvas/*.messages' '/var/log/*log' 
+	'/var/log/*.1' '/var/log/*.gz' '/var/log/*.old' '/var/log/*.err/' 
+	'/var/log/messages' 'var/log/mysql/*.log' '/var/log/mysql/*.gz' 
+	'/var/log/postgresql/*.log' '/var/log/wmtp' '/var/log/*.dat');
+
+	# start backup  
+	IFS=$'\n'
+	for username in $(echo -e "/root/\n$(ls /home/ | sed -e 's_^_/home/_' -e 's_$_/_')"); do
+		echo -e "$cyan[info]$green Backup logs data of $red$username$endc"
+		cd $username
+		sleep 2
+
+		IFS=$','
+		for files in ${log_folder[@]}; do
+			if [ "$(ls -A "$files" 2> /dev/null | wc -l)" -gt 0 ]; then
+				# simple backup with "cp" in /tmp/ directory
+				cp -R "$files" /tmp/backup-logs-$current_date 2> /dev/null
+				echo -e "$cyan[+]$green copied: $white$files$endc"
+			fi
+		done		
+	done
+	
+	# compress backup folder with tar and move backup in /root/backups/ folder
+	echo -e "$cyan[info]$green Compress backup with tar, please wait...$endc"
+	local tmp_dir='/tmp/'
+	cd $tmp_dir
+	if [ "$?" = "0" ]; then
+		tar -czf backup-logs-$current_date.tar.gz backup-logs-$current_date
+		mkdir -p /root/backups
+		mv backup-logs-$current_date.tar.gz /root/backups
+		sleep 5
+	else
+	echo -e "$red[!] An error occurred, please check your configuration$endc"
+		exit 1
+	fi
+	
+	# encrypt new backup with GPG, AES256
+	# gpg -h for more information 
+	echo -e "$cyan[info]$green Encrypt backup with GnuPG, cipher AES256$endc"  
+	cd /root/backups
+	gpg -ca --cipher-algo AES256 backup-logs-$current_date.tar.gz
+	sleep 3
+
+	# remove unnecessary backup files
+	echo -e "$cyan[info]$green Backup encrypted, remove old backup files$endc"
+	rm backup-logs-$current_date.tar.gz
+	rm -R /tmp/backup-logs-*
+	sleep 3
+
+	echo -e "$cyan[+]$green Backup complete:$white /root/backups/backup-logs-$current_date.tar.gz.asc"
+	exit 0
+}
+
+
 # run bleachbit command line interface
 function run_bleachbit {
-	echo -e "\n$cyan[info]$green Starting bleachbit cleaner$RESETCOLOR\n"
+	echo -e "\n$cyan[info]$green Starting bleachbit cleaner$endc"
 	sleep 1
 
 	bleachbit --overwrite --clean apt.autoremove apt.clean bash.history \ 
@@ -84,17 +164,16 @@ function run_bleachbit {
 	sleep 1
 	printf "\033c"
 
-	echo -e "\n$cyan[+]$green Temporary files are deleted$RESETCOLOR"
+	echo -e "$cyan[+]$green Temporary files are deleted$endc"
 }
 
 
 # delete logs with srm 
 function secure_rm {	
-	echo -e "\n$cyan[info]$green Starting secure file deletion with srm, this will take some time...$RESETCOLOR"
+	echo -e "$cyan[info]$green Starting secure file deletion with srm, this will take some time...$endc"
 	
-	# array for tools and logs folders
+	# list logs in root and user home directory  
 	declare -a log_folder=(
-	# root and user Home directory
 	'/nmap_output/' '/.maltego/*BT/var/cache/*' '/.maltego/*BT/var/log/*' 
 	'/.maltego/*CaseFileCE/var/cache/*' '/.maltego/*CaseFileCE/var/cache/*'
 	'./chirp/*.log' '*.mtgrx' '/.recon-ng/*' '*.sprt' '/*-tool-output/' 
@@ -111,34 +190,48 @@ function secure_rm {
 	'/var/log/messages' 'var/log/mysql/*.log' '/var/log/mysql/*.gz' 
 	'/var/log/postgresql/*.log' '/var/log/wmtp' '/var/log/*.dat');
 
-	# root and user logs
+	# delete logs
 	IFS=$'\n'
 	for username in $(echo -e "/root/\n$(ls /home/ | sed -e 's_^_/home/_' -e 's_$_/_')"); do
-		echo -e "\n$cyan[info]$green Deleting logs of $red$username$RESETCOLOR"
+		echo -e "$cyan[info]$green Deleting logs of $red$username$endc"
 		cd $username
 		sleep 2
 
 		IFS=$','
-		for i in ${log_folder[@]}; do
-			if [ "$(ls -A "$i" 2> /dev/null | wc -l)" -gt 0 ]; then
+		for files in ${log_folder[@]}; do
+			if [ "$(ls -A "$files" 2> /dev/null | wc -l)" -gt 0 ]; then
 				# execute command "srm -i -D -R <file/folder>"
 				# -i, --interactive     prompt before any removal
 				# -D, --dod             overwrite with 7 US DoD compliant passes
 				# -r, -R, --recursive   remove the contents of directories
-				srm -i -D -R "$i" 2> /dev/null
-				echo -e "$cyan[+]$green deleted: $white $i $RESETCOLOR"
+				srm -i -D -R "$files" 2> /dev/null
+				echo -e "$cyan[+]$green deleted: $white$files$endc"
 			fi
 		done
 	done
 
 	# delete dmesg logs
-	echo -e "\n$green Delete kernel messages? [Y/n]:$RESETCOLOR"
-	read -e yno
+	echo -e "$green Delete kernel messages? [Y/n]: $endc" 
+	read -e yno	
 	case $yno in
 		[yY]|[y|Y] )
 			dmesg -C
 			sleep 3
-			echo -e "$cyan[+]$green dmesg deleted$RESETCOLOR\n"
+			echo -e "$cyan[+]$green dmesg deleted$endc"
+			;;
+		*)		
+			;;	
+	esac
+
+	# bleachbit don't delete "/root/.bash_history" file if you don't run
+	# from sudo, delete with "history -c" command
+	echo -e "$green Delete root bash history? [Y/n]: $endc" 
+	read -e yno	
+	case $yno in
+		[yY]|[y|Y] )
+			history -c
+			sleep 1
+			echo -e "$cyan[+]$green bash history deleted$endc"
 			;;
 		*)		
 			;;	
@@ -148,26 +241,26 @@ function secure_rm {
 
 # emptying the buffers cache (free pagecache, entries and inodes)
 function drop_cache {
-	echo -e "$cyan[info]$green Drop data from RAM$RESETCOLOR\n"
+	echo -e "$cyan[info]$green Drop data from RAM$endc"
 	sh -c 'echo 3 >/proc/sys/vm/drop_caches'
-	echo -e "$cyan[+]$green RAM empty$RESETCOLOR"
+	echo -e "$cyan[+]$green RAM empty$endc"
 	sleep 5
 	printf "\033c"
 
-	echo -e "$cyan[ ok ]$white All logs and files are securely deleted and your System is clean$RESETCOLOR\n"
+	echo -e "$cyan[ ok ]$white All logs and files are securely deleted and your System is clean$endc\n"
 }
 
 
 # ask for reboot
 function system_reboot {
-	echo -e "\n$green It is recommended to reboot system, reboot now? [Y/n]:$RESETCOLOR"
-	read -e yno
+	echo -e "$green It is recommended to reboot system, reboot now? [Y/n]: $endc" 
+	read -e yno	
 	case $yno in
 		[yY]|[y|Y] )
 			reboot
 			;;
 		*)
-		echo -e "\n$green Exit!$RESETCOLOR"
+		echo -e "$green Exit!$endc"
 			exit 0
 			;;
 	esac
@@ -181,10 +274,10 @@ function start {
 
 	# check if dependencies are installed 
 	command -v bleachbit -h > /dev/null 2>&1 ||
-	{ echo -e "\n$red[!] bleachbit isn't installed, exiting...$RESETCOLOR"; exit 0; }
+	{ echo -e "$red[!] bleachbit isn't installed, exiting...$endc"; exit 0; }
 
 	command -v srm --help > /dev/null 2>&1 ||
-	{ echo -e "\n$red[!] srm isn't installed, exiting...$RESETCOLOR"; exit 0; }
+	{ echo -e "$red[!] srm isn't installed, exiting...$endc"; exit 0; }
 
 	run_bleachbit
 	secure_rm
@@ -193,37 +286,46 @@ function start {
 }
 
 
-# print help menu'
-function help_menu {
-	banner 
-	echo -e "\n$white Usage:
-
-┌─╼ $red$USER$white ╺─╸ $red$(hostname)$white
-└───╼ $green""./kalieraser.sh $white[ $green""start$white | $green""help $white""]
-
-$white start$red -$green Start the program 
-
-$white help$red -$green Show this help message and exit 
-$RESETCOLOR" >&2
-	exit 1
-
-	echo -e $RESETCOLOR
-
+# display program and srm version then exit
+function print_version {
+	echo -e "$white $program $version$endc"
+	echo -e "$white $(srm -V)$endc"
 	exit 0
 }
 
 
-# menu options 
+# print help menu'
+function help_menu {
+	banner
+	echo -e "\n$white Usage:$endc\n"
+	echo -e "$white┌─╼ $red$USER$white ╺─╸ $red$(hostname)$endc"
+	echo -e "$white└───╼ $green$program <argument>$endc\n"
+
+	echo -e "$white Arguments:$endc\n"
+	echo -e "$red help    $green show this help message and exit$endc"
+	echo -e "$red start   $green start program and delete logs$endc"
+	echo -e "$red backup  $green backup data and log folders$endc"
+	echo -e "$red version $green display program and srm version$endc"
+	exit 0
+}
+
+
+# cases user input
 case "$1" in
 	start)
 		start
 		;;
+	backup)
+		backup_data
+		;;
+	version)
+		print_version
+		;;
 	help)
 		help_menu
 		;;
-    *)
+	*)
 help_menu
-;;
-esac
+exit 1
 
-exit 0
+esac
